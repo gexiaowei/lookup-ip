9e251757f43d531df6c54f6cff8d9956cba004ee
gexiaowei <gandxiaowei@gmail.com>
2017-10-20 15:07:05 +0800

    update look up ip for lan and www

--- lookup.py
+++ lookup.py
@@ -3,17 +3,55 @@
 # Create time @ 2016-03-27 15:03:56
 
 import sys
+import socket
 from workflow import Workflow, ICON_WEB, web
 
 API = "http://ip-api.com/json"
 
+
 def main(wf):
+    www_ip = get_www_ip()
+    wf.add_item(
+        title=www_ip['ip'],
+        subtitle=www_ip['timezone'],
+        arg=www_ip['ip'],
+        valid=True,
+        icon=ICON_WEB)
+
+    lan_ip = get_lan_ip()
+    wf.add_item(
+        title=lan_ip,
+        subtitle=lan_ip,
+        arg=lan_ip,
+        valid=True,
+        icon=ICON_WEB)
+
+    wf.send_feedback()
+
+
+def get_www_ip():
     data = web.get(API).json()
     if data['status'] == 'success':
-        wf.add_item(title=data['query'],
-                     subtitle=data['timezone'],
-                     icon=ICON_WEB)
-    wf.send_feedback()
+        return {
+            "ip": data['query'],
+            "timezone": data['timezone']
+        }
+    else:
+        return {
+            "ip": "unknown address",
+        }
+
+
+def get_lan_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except:
+        ip = "unknown address"
+    finally:
+        s.close()
+    return ip
 
 
 if __name__ == '__main__':
